d39e0518a9c6bf01e313af10d0f45c67bd0235b9
yuu-ux <yuugaehara@gmail.com>
2025-07-31 16:56:57 +0900

    :wrench: 391 解き直し

--- ABC391/SeekGrid.py
+++ ABC391/SeekGrid.py
@@ -1,28 +1,35 @@
+N, M = map(int, input().split())
+
+S = [input() for _ in range(N)]
+T = [input() for _ in range(M)]
+
+def same_grid(S, T, i, j):
+    M = len(T)
+    for k in range(M):
+        for l in range(M):
+            if S[i + k][j + l] != T[k][l]:
+                return False
+    return True
+
+def seek_grid(S, T):
+    for i in range(N - M + 1):
+        for j in range(N - M + 1):
+            if same_grid(S, T, i, j):
+                return i+1, j+1
+
+print(*(seek_grid(S, T)))
+
 # N, M = map(int, input().split())
-#
 # S = [input() for _ in range(N)]
 # T = [input() for _ in range(M)]
 #
 # for i in range(N - M + 1):
-#     matched = True
-#     for j in range(N - M + 1):
-#         if S[i][j]
-#             break
+#     for col in range(len(S[i]) - len(T[0]) + 1):
+#         matched = True
+#         for j in range(M):
+#             if col + len(T[j]) > len(S[i + j]) or S[i + j][col:col + len(T[j])] != T[j]:
+#                 matched = False
+#                 break
 #         if matched:
-#             print(i + 1, index + 1)
+#             print(i + 1, col + 1)
 #             exit()
-
-N, M = map(int, input().split())
-S = [input() for _ in range(N)]
-T = [input() for _ in range(M)]
-
-for i in range(N - M + 1):
-    for col in range(len(S[i]) - len(T[0]) + 1):
-        matched = True
-        for j in range(M):
-            if col + len(T[j]) > len(S[i + j]) or S[i + j][col:col + len(T[j])] != T[j]:
-                matched = False
-                break
-        if matched:
-            print(i + 1, col + 1)
-            exit()
