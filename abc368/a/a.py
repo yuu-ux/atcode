a631b6a6cfb111f5b47272d8b6fbfe3ed3bb5c22
yuu-ux <yuugaehara@gmail.com>
2025-08-14 23:22:45 +0900

    :wrench: 別解

--- abc368/a/a.py
+++ abc368/a/a.py
@@ -1,5 +1,8 @@
-N, K = map(int,input().split())
-A = list(map(int, input().split()))
-
-tail = A[-K:]
-print(*(tail + A[:-K]))
+# N, K = map(int,input().split())
+# A = list(map(int, input().split()))
+#
+# tail = A[-K:]
+# print(*(tail + A[:-K]))
+N, K = map(int, input().split())
+A = list(input().split())
+print(*(A[N-K:]+A[:N-K]))
