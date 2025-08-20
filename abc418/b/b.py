d1d38cf7ce2c1f7d9ec0bd74a0207992bad46f78
yuu-ux <yuugaehara@gmail.com>
2025-08-20 18:55:17 +0900

    :heavy_plus_sign: 418_b 解答

--- abc418/b/b.py
+++ abc418/b/b.py
@@ -1,22 +1,16 @@
 S = list(input())
+N = len(S)
 
-s_len = len(S)
-res = 0
-x = 0
-start = 0
-end = 0
-t_len = 0
-for i in range(s_len):
-    if S[i] == 't':
-        x += 1
-        start = i
-        for j in range(i+1, s_len+1):
-            if S[j] == 't':
-                end = j
-                t_len = end - start + 1
-                temp = (x-2)/(t_len-2)
-                if res < temp:
-                    res = temp
-                break
-
-print(res)
+ans = 0
+for r in range(N):
+    for l in range(0, r-1):
+        if S[l] != 't' or S[r] != 't':
+            continue
+        cnt = 0
+        _len = r - l - 1
+        for k in range(l+1, r):
+            if S[k] == 't':
+                cnt += 1
+        now = cnt / _len
+        ans = max(ans, now)
+print(ans)
